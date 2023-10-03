889d55d0e104be94c38b4ce247d27cc9c00a2738
asjadanis <asjadanis128@gmail.com>
2023-10-03 15:58:42 -0500

    fix: streaming error

--- HackernewsStreaming/src/main/java/consumer/HackerNewsSparkConsumer.java
+++ HackernewsStreaming/src/main/java/consumer/HackerNewsSparkConsumer.java
@@ -29,9 +29,13 @@ public class HackerNewsSparkConsumer {
         public String type; // Type of post (e.g., "story")
         public String url; // URL of the post
     }
+    
+    private static Admin admin;
+	private static Configuration config;
+	private static Connection connection;
 
     @SuppressWarnings("deprecation")
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         SparkConf sparkConf = new SparkConf().setAppName("HackerNewsConsumer").setMaster("local[*]");
         JavaStreamingContext streamingContext = new JavaStreamingContext(sparkConf, Durations.seconds(2));
 
@@ -41,82 +45,86 @@ public class HackerNewsSparkConsumer {
         kafkaParams.put("value.deserializer", StringDeserializer.class);
         kafkaParams.put("group.id", "hacker-news-group");
         kafkaParams.put("auto.offset.reset", "latest");
+        kafkaParams.put("enable.auto.commit", false);
 
         Collection<String> topics = Arrays.asList("hacker_news_topic");
-
-        JavaInputDStream<ConsumerRecord<String, String>> stream =
-                KafkaUtils.createDirectStream(
-                        streamingContext,
-                        LocationStrategies.PreferConsistent(),
-                        ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
-                );
-
-        JavaDStream<String> lines = stream.map(ConsumerRecord::value);
-
-        JavaDStream<HackerNewsPost> posts = lines.map(json -> new Gson().fromJson(json, HackerNewsPost.class));
-
-        posts.foreachRDD(rdd -> {
-            rdd.foreachPartition(records -> {
-                Configuration config = HBaseConfiguration.create();
-                Connection connection = null;
-                Admin admin = null;
-                final Table[] tableContainer = new Table[1];
-
-                try {
-                    connection = ConnectionFactory.createConnection(config);
-                    admin = connection.getAdmin();
-                    TableName tableName = TableName.valueOf("hacker_news_posts");
-
-                    if (!admin.tableExists(tableName)) {
-                        // Define table descriptor
-                        HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
-                        
-                        // Define column family descriptor
-                        HColumnDescriptor columnDescriptor = new HColumnDescriptor("post");
-                        
-                        // Add column family to table descriptor
-                        tableDescriptor.addFamily(columnDescriptor);
-                        
-                        // Create table
-                        admin.createTable(tableDescriptor);
-                        System.out.println("Table and column family created");
-                    }
-
-                    tableContainer[0] = connection.getTable(tableName);
-
-                    records.forEachRemaining(post -> {
-                        Put put = new Put(Bytes.toBytes(post.title));
+        
+        JavaDStream<String> stream = KafkaUtils.createDirectStream(
+        		streamingContext,
+                LocationStrategies.PreferConsistent(),
+                ConsumerStrategies.<String, String>Subscribe(topics, kafkaParams)
+        ).map(record -> record.value());
+        
+        setupHBaseTable();
+        
+        stream.foreachRDD(rdd -> {
+        	rdd.foreach(entry -> {
+        		Gson geo = new Gson();
+            	HackerNewsPost post = geo.fromJson(entry, HackerNewsPost.class);
+            	
+            	if(post != null){
+            		try (Table table = connection.getTable(TableName.valueOf("hacker_news_posts"))) {
+                        Put put = new Put(Bytes.toBytes(post.id));
                         put.addColumn(Bytes.toBytes("post"), Bytes.toBytes("author"), Bytes.toBytes(post.by));
-                        put.addColumn(Bytes.toBytes("post"), Bytes.toBytes("url"), Bytes.toBytes(post.url));
-                        try {
-                            tableContainer[0].put(put);
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    try {
-                        if (admin != null) {
-                            admin.close();
-                        }
-                        if (tableContainer[0] != null) {
-                            tableContainer[0].close();
-                        }
-                        if (connection != null) {
-                            connection.close();
-                        }
+                        put.addColumn(Bytes.toBytes("post"), Bytes.toBytes("score"), Bytes.toBytes(post.score));
+                        //put.addColumn(Bytes.toBytes("post"), Bytes.toBytes("url"), Bytes.toBytes(post.url));
+                        table.put(put);
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+            	}
+        	});
+        });
+        
+        JavaDStream<Double> scores = stream.map(entry -> {
+            Gson gson = new Gson();
+            HackerNewsPost post = gson.fromJson(entry, HackerNewsPost.class);
+            return (double) post.score;
+        });
+        
+        scores.foreachRDD(rdd -> {
+            if (!rdd.isEmpty()) {
+                double avgScore = rdd.reduce((a, b) -> a + b) / rdd.count();
+                try(Table table = connection.getTable(TableName.valueOf("hacker_news_posts"))){
+                	Put put = new Put(Bytes.toBytes("analytics"));
+                    put.addColumn(Bytes.toBytes("analysis"), Bytes.toBytes("avg_score"), Bytes.toBytes(avgScore));
+                	table.put(put);
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-
-            });
+            }
         });
 
         streamingContext.start();
         streamingContext.awaitTermination();
     }
+    
+    @SuppressWarnings("deprecation")
+	public static void setupHBaseTable() throws IOException {
+    	config = HBaseConfiguration.create();
+    	connection = ConnectionFactory.createConnection(config);
+    	admin = connection.getAdmin();
+    	
+    	try {
+    		
+    		TableName tableName = TableName.valueOf("hacker_news_posts");
+    		@SuppressWarnings("deprecation")
+			HTableDescriptor tableDescriptor = new HTableDescriptor(tableName);
+    		
+    		// Define column family descriptor
+            @SuppressWarnings("deprecation")
+			HColumnDescriptor columnDescriptor = new HColumnDescriptor("post");
+            
+            // Add column family to table descriptor
+            tableDescriptor.addFamily(columnDescriptor);
+            
+            // Create table
+            admin.createTable(tableDescriptor);
+            
+            System.out.println("Table and column family created");
+    	} catch (Exception e) {
+    		System.out.println("Exception creating table: " + e.getMessage());
+    	}
+    	 
+    }
 }
-
